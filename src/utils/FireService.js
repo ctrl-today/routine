c83158243047d6a768565e4a7b96cea8fd81c896
gelus <benjarmil@gmail.com>
2020-07-07 20:32:33 -0600

    change firebase include method to use module bundlers

--- src/utils/FireService.js
+++ src/utils/FireService.js
@@ -1,3 +1,22 @@
+import * as firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+// Routine firebase UNIQUE NON-SECRET CONFIG OBJECT 
+// ( Do not adjust, you silly pickle )
+const firebaseConfig = {
+  apiKey: "AIzaSyAggBp8qB7ecmxNXYtmiWLrRjhJtT7E0YA",
+  authDomain: "ctrl-routine.firebaseapp.com",
+  databaseURL: "https://ctrl-routine.firebaseio.com",
+  projectId: "ctrl-routine",
+  storageBucket: "ctrl-routine.appspot.com",
+  messagingSenderId: "872709776681",
+  appId: "1:872709776681:web:3bb93335adf9c213e1c338",
+  measurementId: "G-HGKXE516KP"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 export class FireService {
 	constructor() {
